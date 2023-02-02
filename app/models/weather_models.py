fcdceddee9407ed931a2d290de6eda7fbe0f34e5
riseryan89 <rise.ryan.lee@gmail.com>
2023-02-02 21:19:08 +0900

    Ch03 Clip03

--- app/models/weather_models.py
+++ app/models/weather_models.py
@@ -3,15 +3,62 @@ from django.db import models
 from app.models import User
 
 
-class Location(models.Model):
-    location = models.CharField(max_length=128, help_text="종관기상관측 센터 지역이름")
+class StationLocation(models.Model):
+    class Provinces(models.TextChoices):
+        SEOUL = "SEOUL", "서울"
+        GYUNGGI = "GG", "경기도"
+        INCHOEN = "INCHOEN", "인천"
+        JEJU = "JEJU", "제주"
+        GANGWON = "GW", "강원도"
+        CHUNGBUK = "CB", "충북"
+        CHUNGNAM = "CN", "충남"
+        GYEONGBUK = "GB", "경북"
+        GYEONGNAM = "GN", "경남"
+        JEONBUK = "JB", "전북"
+        JEONNAM = "JN", "전남"
+        DAEGU = "DAEGU", "대구"
+        DAESAN = "DAESAN", "대전"
+        BUSAN = "BUSAN", "부산"
+        ULSAN = "ULSAN", "울산"
+        SEJONG = "SEJONG", "세종"
+
+    station_name = models.CharField(max_length=128, help_text="종관기상관측 센터 지역이름")
+    kma_station_code = models.CharField(max_length=8, help_text="종관기상관측 센터 코드")
+    station_authority = models.CharField(max_length=128, help_text="종관기상관측 센터 관할지역")
+    province = models.CharField(max_length=8, choices=Provinces.choices, help_text="종관기상관측 센터 관할지역")
+    late_models_changed_at = models.DateTimeField(null=True, blank=True, help_text="최근 예측모델 변경 시간")
+    models_name = models.CharField(max_length=128, null=True, blank=True, help_text="예측모델 파일명")
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
+    """
+        239	세종	대전지방기상청
+        105	강릉	강원지방기상청
+        108	서울	수도권기상청
+        189	서귀포  제주지방기상청
+        295	남해	부산지방기상청
+    """
+
+    def __str__(self):
+        return f"{self.station_name} - {self.station_authority}"
+
+    class Meta:
+        verbose_name_plural = "StationLocations"
 
 
 class Weather(models.Model):
-    location = models.ForeignKey("Location", on_delete=models.CASCADE, related_name="weather_location")
+    location = models.ForeignKey("StationLocation", on_delete=models.CASCADE, related_name="weather_location")
     date = models.DateField()
-    max_temp = models.FloatField()  # maxTa
+    min_temp = models.FloatField()
+    max_temp = models.FloatField()
+    rain_hour = models.FloatField(default=0)
+    total_rain = models.FloatField(default=0)
+    avg_humidity = models.FloatField()
+    wind_speed = models.FloatField()
+    wind_direction = models.FloatField()
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        indexes = [
+            models.indexes.Index(fields=["location", "date"], name="ix_weather_location_date_time"),
+        ]
