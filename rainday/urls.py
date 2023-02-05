98bc26174a3cb596fcb8f5926fe436a41d3acaaf
riseryan89 <rise.ryan.lee@gmail.com>
2023-02-05 20:59:10 +0900

    Ch07 Clip01

--- rainday/urls.py
+++ rainday/urls.py
@@ -16,8 +16,23 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 from app import urls
+from django.contrib.auth import views as a_v
+
 
 urlpatterns = [
     path("admin/", admin.site.urls),
+    path("accounts", include("django.contrib.auth.urls")),
+    path("auth/pw-change", a_v.PasswordChangeView.as_view(template_name="pw-change.html"), name="pw-change"),
     path("", include(urls)),
 ]
+
+"""
+accounts/ login/ [name='login']
+accounts/ logout/ [name='logout']
+accounts/ password_change/ [name='password_change']
+accounts/ password_change/done/ [name='password_change_done']
+accounts/ password_reset/ [name='password_reset']
+accounts/ password_reset/done/ [name='password_reset_done']
+accounts/ reset/<uidb64>/<token>/ [name='password_reset_confirm']
+accounts/ reset/done/ [name='password_reset_complete']
+"""
