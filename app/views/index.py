aa23a1faa3e773c0d374b70407acc31dd7741d64
riseryan89 <rise.ryan.lee@gmail.com>
2023-02-05 18:43:37 +0900

    Ch06 Clip07

--- app/views/index.py
+++ app/views/index.py
@@ -6,6 +6,7 @@ from django.contrib.auth import authenticate, login
 from django.shortcuts import render, redirect
 
 from app.forms.auth_forms import LoginForm, SignupForm
+from app.forms.subscribe_form import LocationSubscribeForm
 from app.models import StationLocation, Weather
 from app.schedulers.prediction import create_model, predict
 from app.schedulers.weather_data_collector import collect_load_weather_data
@@ -34,6 +35,11 @@ def index(request):
 
     station_location = StationLocation.objects.get(kma_station_code=kma_station_code)
     last_5_data = Weather.objects.filter(location=station_location).order_by("-date")[:5]
+    if request.user.is_authenticated:
+        subscribed_locations = request.user.locations.all().values_list("id", flat=True)
+        subscribe_form = LocationSubscribeForm(initial={"checkbox_field": list(subscribed_locations)})
+    else:
+        subscribe_form = LocationSubscribeForm()
     context = {
         "hello": "hello world",
         "region": region,
@@ -42,12 +48,16 @@ def index(request):
         "last_5_data": last_5_data,
         "auth_form": LoginForm(),
         "signup_form": SignupForm(),
+        "subscribe_form": subscribe_form,
         "locations": all_location,
         "form_error": "",
+        "initial_data": [],
         "prediction": predict(station_location),
     }
+
     if request.method == "POST":
         submit_type = request.GET.get("type")
+
         if submit_type == "signup":
             context["signup_form"] = SignupForm(data=request.POST)
             form = SignupForm(data=request.POST)
@@ -56,6 +66,7 @@ def index(request):
                 return redirect("home")
             else:
                 context["form_error"] = "signup"
+
         elif submit_type == "login":
             form = LoginForm(data=request.POST)
             context["auth_form"] = LoginForm(data=request.POST)
@@ -68,7 +79,18 @@ def index(request):
                     return redirect("home")
             else:
                 context["form_error"] = "login"
+
+        elif submit_type == "subscription":
+            form = LocationSubscribeForm(data=request.POST)
+            selected_locations = form.data.get("checkbox_field", [])
+            request.user.locations.clear()
+            if selected_locations:
+                location = StationLocation.objects.filter(id__in=selected_locations)
+                request.user.locations.set(location)
+            return redirect("home")
+
         return render(request, "index.html", context)
+
     else:
         return render(request, "index.html", context)
 
